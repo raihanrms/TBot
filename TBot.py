1298b2ea4478a8578e7fa5c9fb438bc2dfeffab8
Raihan Munim <raihan.srizon@gmail.com>
2022-05-31 02:07:22 +0600

    updates

--- TBot.py
+++ TBot.py
@@ -1,21 +1,36 @@
-import os
-from click import command
 import telebot
+import os
+import parse
+
+from click import command
+from urllib.parse import urlparse
+
+PARSE_MODE = "MarkdownV2"
+AVAILABLE_COMMANDS = ["/start", "/help", "/test"]
+
+STARTER = """
+Hi\! I am RmsBot\, a bot built by [raihanrms](https://github.com/raihanrms)
+`This is a work in progress bot, that can save time for \open-source researchers and volenteers`"""
 
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['greet'])
+@bot.message_handler(commands=["start"])
 def greet(message):
-    bot.reply_to(message, "Hey there!")
+    bot.send_message(message.chat.id, STARTER, parse_mode=PARSE_MODE)
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
     bot.send_message(message.chat.id, "hello")
 
-# Custom function to fetch a video link with video id
-# @bot.message_handler(func=fetch_ytlink)
-# def send_link(message):
-#     pass
+@bot.message_handler(commands=["test"])
+def test(message):
+    bot.send_message(message.chat.id, "*I'm up\!*", parse_mode=PARSE_MODE)
+
+@bot.message_handler(commands=['help'])
+def greet(message):
+    bot.reply_to(message, "Hey there!")
+
+
 
 bot.infinity_polling()
