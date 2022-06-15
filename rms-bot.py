ff4d1b89e75b2c57c22e33403875c4b2b5b860f6
Raihan Munim <raihan.srizon@gmail.com>
2022-06-16 04:17:09 +0600

    issues remain

--- rms-bot.py
+++ rms-bot.py
@@ -27,7 +27,7 @@ def start(update, context):
     This function sets the language of the bot."""
     
     # Create the buttons for language selection
-    keyboard = [['BN', 'EN']]
+    keyboard = [['Bengali', 'English']]
 
     # Create initial message
     message = "Hey, I'm RMS-Bot! / আসসালামু আলাইকুম, আমি আরএমএস-বট! \n\n Please select a language. / অনুগ্রহ করে ভাষা নির্বাচন করুন।"
@@ -40,9 +40,9 @@ def start(update, context):
 
 def set_lang(bot, update):
     """
-    First handler to receive the language globally.
+    First handler with received data to set language globally.
     """
-    # set the language
+    # Set language:
     global LANG
     LANG = update.message.text
     user = update.message.from_user
@@ -50,4 +50,203 @@ def set_lang(bot, update):
     logger.info("Language set by {} to {}.".format(user.first_name, LANG))
     update.message.reply_text(lang_selected[LANG],
                               reply_markup=ReplyKeyboardRemove())
+
     return MENU
+
+
+def menu(bot, update):
+    """
+    Main menu function.
+    This will display the options from the main menu.
+    """
+    # Create buttons to slect language:
+    keyboard = [[send_report[LANG], view_map[LANG]],
+                [view_faq[LANG], view_about[LANG]]]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+
+    user = update.message.from_user
+    logger.info("Menu command requested by {}.".format(user.first_name))
+    update.message.reply_text(main_menu[LANG], reply_markup=reply_markup)
+
+    return SET_STAT
+
+
+def set_state(bot, update):
+    """
+    Set option selected from menu.
+    """
+    # Set state:
+    global STATE
+    user = update.message.from_user
+    if update.message.text == send_report[LANG]:
+        STATE = REPORT
+        report(bot, update)
+        return LOCATION
+    elif update.message.text == view_map[LANG]:
+        STATE = MAP
+        vmap(bot, update)
+        return MENU
+    elif update.message.text == view_faq[LANG]:
+        STATE = FAQ
+        faq(bot, update)
+        return MENU
+    elif update.message.text == view_about[LANG]:
+        STATE = ABOUT
+        about_bot(bot, update)
+        return MENU
+    else:
+        STATE = MENU
+        return MENU
+
+
+def report(bot, update):
+    """
+    FAQ function. Displays FAQ about disaster situations.
+    """
+    user = update.message.from_user
+    logger.info("Report requested by {}.".format(user.first_name))
+    update.message.reply_text(loc_request[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return
+
+
+def location(bot, update):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Location of {}: ({}, {})".format(
+                user.first_name, user_location.latitude,
+                user_location.longitude))
+    report_map = geo_app()
+    report_map.append_data(user_location.latitude, user_location.longitude)
+    update.message.reply_text(loc_aquired[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return MENU
+
+
+def vmap(bot, update):
+    """
+    View map function. In development...
+    """
+    user = update.message.from_user
+    logger.info("Map requested by {}.".format(user.first_name))
+    bot.send_message(chat_id=update.message.chat_id, text=map_info[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+
+    # View map locally:
+    report_map = geo_app()
+    report_map.latlong_to_coords()
+    report_map.visualize()
+    return
+
+
+def faq(bot, update):
+    """
+    FAQ function. Displays FAQ about disaster situations.
+    """
+    user = update.message.from_user
+    logger.info("FAQ requested by {}.".format(user.first_name))
+    bot.send_message(chat_id=update.message.chat_id, text=faq_info[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return
+
+
+def about_bot(bot, update):
+    """
+    About function. Displays info about DisAtBot.
+    """
+    user = update.message.from_user
+    logger.info("About info requested by {}.".format(user.first_name))
+    bot.send_message(chat_id=update.message.chat_id, text=about_info[LANG])
+    bot.send_message(chat_id=update.message.chat_id, text=back2menu[LANG])
+    return
+
+
+def help(bot, update):
+    """
+    Help function.
+    This displays a set of commands available for the bot.
+    """
+    user = update.message.from_user
+    logger.info("User {} asked for help.".format(user.first_name))
+    update.message.reply_text(help_info[LANG],
+                              reply_markup=ReplyKeyboardRemove())
+
+
+def cancel(bot, update):
+    """
+    User cancelation function.
+    Cancel conersation by user.
+    """
+    user = update.message.from_user
+    logger.info("User {} canceled the conversation.".format(user.first_name))
+    update.message.reply_text(goodbye[LANG],
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """
+    Main function.
+    This function handles the conversation flow by setting
+    states on each step of the flow. Each state has its own
+    handler for the interaction with the user.
+    """
+    global LANG
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(telegram_token)
+
+    # Get the dispatcher to register handlers:
+    dp = updater.dispatcher
+
+    # Add conversation handler with predefined states:
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            SET_LANG: [RegexHandler('^(ES|EN)$', set_lang)],
+
+            MENU: [CommandHandler('menu', menu)],
+
+            SET_STAT: [RegexHandler(
+                '^({}|{}|{}|{})$'.format(
+                    send_report['ES'], view_map['ES'],
+                    view_faq['ES'], view_about['ES']),
+                    set_state),
+                    RegexHandler(
+                        '^({}|{}|{}|{})$'.format(
+                            send_report['EN'], view_map['EN'],
+                            view_faq['EN'], view_about['EN']),
+                            set_state)],
+
+            LOCATION: [MessageHandler(Filters.location, location),
+                       CommandHandler('menu', menu)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel),
+                   CommandHandler('help', help)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # Log all errors:
+    dp.add_error_handler(error)
+
+    # Start DisAtBot:
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process
+    # receives SIGINT, SIGTERM or SIGABRT:
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
